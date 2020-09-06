1bd5a670c3ad9a1491c196bb1bb52eb51cc1b53f
Kavya-24 <Kavya_1901cs30@iitp.ac.in>
2020-09-06 23:39:58 +0530

    Add splash screen

--- app/src/main/java/com/example/reflexion/ui/auth/SplashActivity.kt
+++ app/src/main/java/com/example/reflexion/ui/auth/SplashActivity.kt
@@ -1,11 +1,14 @@
 package com.example.reflexion.ui.auth
 
+import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import androidx.appcompat.app.AppCompatActivity
+import com.example.reflexion.MainActivity
 import com.example.reflexion.R
 import com.google.firebase.auth.FirebaseAuth
 
+@Suppress("DEPRECATION")
 class SplashActivity : AppCompatActivity() {
 
 
@@ -22,8 +25,12 @@ class SplashActivity : AppCompatActivity() {
 
             if (FirebaseAuth.getInstance().currentUser == null) {
                 //When we are not signed in
+                val i = Intent(this, LoginActivity::class.java)
+                startActivity(i);
 
             } else {
+                val i = Intent(this, MainActivity::class.java)
+                startActivity(i);
 
             }
 
@@ -32,8 +39,8 @@ class SplashActivity : AppCompatActivity() {
         }, 1000)
 
 
-        }
+    }
 
 
 }
-}
+
