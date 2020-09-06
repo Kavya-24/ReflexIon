6d47e91275638dd2e7474e9ff3190a516aa92a4e
Kavya-24 <Kavya_1901cs30@iitp.ac.in>
2020-09-07 01:07:40 +0530

    set Tasks Adapter

---
+++ app/src/main/java/com/example/reflexion/adapters/TasksAdapter.kt
@@ -0,0 +1,66 @@
+package com.example.reflexion.adapters
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.reflexion.R
+import com.example.reflexion.models.Task
+
+class TasksAdapter(val itemClick: OnItemClickListener) :
+    RecyclerView.Adapter<TasksAdapter.MyViewHolder>() {
+
+
+    //Initialize an empty list of the dataclass T
+    var lst: List<Task> = listOf()
+
+    class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+        val DESC = itemView.findViewById<TextView>(R.id.tv_description)
+        val STARS = itemView.findViewById<TextView>(R.id.tv_stars)
+
+        //Bind a single item
+        fun bindPost(_listItem: Task, itemClick: OnItemClickListener) {
+            with(_listItem) {
+
+                DESC.text = _listItem.description
+                STARS.text = _listItem.numStars.toString()
+
+                itemView.setOnClickListener {
+                    itemClick.clickThisItem(_listItem)
+                }
+
+
+            }
+        }
+
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+
+        val view =
+            LayoutInflater.from(parent.context)
+                .inflate(R.layout.item_task, parent, false)
+        return TasksAdapter.MyViewHolder(view)
+
+    }
+
+    override fun getItemCount(): Int {
+        return lst.size
+
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+
+        holder.bindPost(lst[position], itemClick)
+
+    }
+
+}
+
+
+interface OnItemClickListener {
+    fun clickThisItem(_listItem: Task)
+}
---
+++ app/src/main/java/com/example/reflexion/models/Task.kt
@@ -0,0 +1,9 @@
+package com.example.reflexion.models
+
+data class Task(
+    val description: String,
+    val time: String,
+    val taskId: String,
+    val numStars: String
+
+)
